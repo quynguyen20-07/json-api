a90c4ab3564f80b36d3a919e07da1fbcfc70c1b2
Quy Nguyen <quynguyena52000@gmai.com>
2023-10-08 20:36:39 +0700

    change server to express

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send("Welcome on port 5000");
 });
 
 app.listen(5000, () => {
