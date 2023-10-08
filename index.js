e4e7fa35bf8fe60fb6a1fa8180d729e38b3e71b3
Quy Nguyen <quynguyena52000@gmai.com>
2023-10-08 20:05:53 +0700

    change server to express

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
+// server.use(middlewares);
 
-server.use("/api", router);
+// server.use("/api", router);
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+// const PORT = process.env.PORT || 3000;
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running on port ${PORT}`);
+// });
+
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+app.get("/home", (req, res) => {
+  res.status(200).json("Welcome, your app is working well");
 });
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+// Export the Express API
+module.exports = app;
